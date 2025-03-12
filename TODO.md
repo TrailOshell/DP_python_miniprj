4f73a2a299732f9fc153fd04366959dd8dc87f59
tsomchan <tonnam.dev@gmail.com>
2025-03-12 18:17:58 +0700

    function can find "checkmate"

--- TODO.md
+++ TODO.md
@@ -1,18 +1,18 @@
-- [ ] Write a function
-    - [ ] that takes rows of a chessboard as arguments
-    - [ ] and checks if your King is "in check".
-- [ ] play with...
-    - [ ] Pawns
-    - [ ] Bishops
-    - [ ] Rooks
-    - [ ] Queens
-    - [ ] King.
-- [ ] A piece can only capture the first possible piece that stands on its path.
+- [x] Write a function
+    - [x] that takes rows of a chessboard as arguments
+    - [x] and checks if your King is "in check".
+- [x] play with...
+    - [x] Pawns
+    - [x] Bishops
+    - [x] Rooks
+    - [x] Queens
+    - [x] King.
+- [x] A piece can only capture the first possible piece that stands on its path.
 - [ ] The board can be of different sizes but will remain a square.
-- [ ] There’s only one King and all other pieces are against it.
-- [ ] All the characters that are not used to refer to pieces are considered as empty squares.
-- [ ] The King is considered to be "in check" when an other enemy piece can capture it.  
-    - [ ] When it’s the case, you will print "Success" on the standard output followed by a newline
-    - [ ] otherwise you will print "Fail" followed by a newline.
+- [ ] There’s only one King
+- [x] All the characters that are not used to refer to pieces are considered as empty squares.
+- [x] The King is considered to be "in check" when an other enemy piece can capture it.  
+    - [x] When it’s the case, you will print "Success" on the standard output followed by a newline
+    - [x] otherwise you will print "Fail" followed by a newline.
 - [ ] Your function should never crash or loop indefinitely.
 - [ ] Whenever an undefiend behavior occurs you should return an error message or prints nothing and gives back control.
--- ex00/main.py
+++ ex00/main.py
@@ -1,35 +1,148 @@
 #!/usr/bin/python3
 
-def check_board(board):
-    if 'K' not in board:
-        return "ERROR: 'K' not in board"
-    w = 0
-    h = 0
-    w_max = 0
-    for c in board:
-        w_max += 1
-        if c == '\n':
-            break
-    for c in board:
-        if c == '\n':
-            h += 1
-            if w != w_max:
-                return f"ERROR: uneven board size (width {w} and width {w_max})"
+# Write a function they said. Not functions with an "S" so if someone want to play a strict guy then here you go. a function
+def checkmate(board):
+    try:
+        if 'K' not in board:
+            return "ERROR: 'K' not in board"
+            # return "ERROR: multiple 'K' found"
 
-        else:
-            w += 1
-    return board
+        w = 0
+        h = 0
+        w_max = 0
+        h_max = 0
+        for c in board:
+            if c == '\n':
+                break
+            w_max += 1
+        for c in board:
+            if c == '\n':
+                h += 1
+                if w != w_max:
+                    return f"ERROR: uneven board size (width {w} and width_max {w_max})"
+                w = 0
+            else:
+                w += 1
+        h += 1
+        h_max = h
+
+        rows, cols = (h, w)
+        arr = [[0 for i in range(cols)] for j in range(rows)]
+        k = [0, 0]
+        w = 0
+        h = 0
+        for c in board:
+            # print(f"{h} {w} {c}")
+            if c == '\n':
+                h += 1
+                w = 0
+            else:
+                if c == 'K':
+                    k = [h, w]
+                if c == 'K' or c == 'P' or c == 'Q' or c == 'R' or c == 'B':
+                    arr[h][w] = c
+                else:
+                    arr[h][w] = '.'
+                w += 1
+
+        for row in arr:
+            for c in row:
+                print(c, end='')
+            print('\n', end='')
+        # print(f"K pos is {k_pos[0]} {k_pos[1]}")
+
+        checkmate = 0
+
+        
+        step = 1
+        left_max = k[0]
+        right_max = w_max - (k[0] + 1)
+        up_max = k[1]
+        down_max = h_max - (k[1] + 1)
+        print(f"King position: {k[0]} {k[1]}")
+        print(f"max steps:\tl {left_max} r {right_max} u {up_max} d {down_max}")
+
+        if arr[k[0] - 1][k[1] + 1] == 'P' or arr[k[0] + 1][k[1] + 1] == 'P':
+            print("A pawn is checking the king")
+            checkmate = 1
+        
+        l = k[0]
+        r = k[0]
+        u = k[1]
+        d = k[1]
+        while step <= left_max or step <= right_max or step <= up_max or step <= down_max:
+            if step <= left_max:
+                l -= 1
+            if step <= right_max:
+                r += 1
+            if step <= up_max:
+                u -= 1
+            if step <= down_max:
+                d += 1
+            print(f"step {step}:\t\tl {l} r {r} u {u} d {d}")
+            if arr[l][k[1]] == 'R' or arr[l][k[1]] == 'Q':
+                checkmate = 1
+            elif arr[l][k[1]] != '.':
+                left_max = step
+            if arr[r][k[1]] == 'R' or arr[r][k[1]] == 'Q':
+                checkmate = 1
+            elif arr[r][k[1]] != '.':
+                right_max = step
+            if arr[k[0]][u] == 'R' or arr[k[0]][u] == 'Q':
+                checkmate = 1
+            elif arr[k[0]][u] != '.':
+                up_max = step
+            if arr[k[0]][d] == 'R' or arr[k[0]][d] == 'Q':
+                checkmate = 1
+            elif arr[k[0]][d] != '.':
+                down_max = step
+            if arr[l][u] == 'B' or arr[l][u] == 'Q':
+                checkmate = 1
+            elif arr[l][u] != '.':
+                left_max = step
+                up_max = step
+            if arr[r][u] == 'B' or arr[r][u] == 'Q':
+                checkmate = 1                       
+            elif arr[r][u] != '.':
+                right_max = step
+                up_max = step
+            if arr[l][d] == 'B' or arr[l][d] == 'Q':
+                checkmate = 1                       
+            elif arr[l][d] != '.':
+                left_max = step
+                down_max = step
+            if arr[r][d] == 'B' or arr[r][d] == 'Q':
+                checkmate = 1
+            elif arr[r][d] != '.':
+                right_max = step
+                down_max = step
+            step += 1
+
+        if checkmate == 1:
+            return "Success"
+        elif checkmate == 0:
+            return "Fail"
+    except:
+        return "Error"
 
 def main():
     board = """\
-    R...
-    .K..
-    ..P.
-    ....\
-    """
-    print("run")
-    print(check_board(board))
+R...
+.K..
+..P.
+....\
+"""
+    # print(board)
+    # print(check_board(board))
     # checkmate(board)
+
+    board2 = """\
+....
+.K..
+....
+.Q..\
+"""
+    print(checkmate(board2))
 
 if __name__ == "__main__":
     main()
