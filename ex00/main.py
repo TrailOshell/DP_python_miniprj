aea1eee06eaeaad097d9b4383d5b2ac02842434d
tsomchan <tonnam.dev@gmail.com>
2025-03-13 09:28:07 +0700

    for loop directions , show grid array number, compact some lines

--- ex00/main.py
+++ ex00/main.py
@@ -11,99 +11,89 @@ def checkmate(board):
     w, h = 0, 0
     w_max, h_max = 0, 0
     for c in board:
-        if c == '\n':
-            break
+        if c == '\n': break
         w_max += 1
     for c in board:
         if c == '\n':
-            h += 1
             if w != w_max:
                 return f"ERROR: uneven board size (last width = {w}, max width = {w_max})"
-            w = 0
-        else:
-            w += 1
-    h += 1
-    h_max = h
+            h += 1; w = 0
+        else: w += 1
+    h += 1; h_max = h
 
-    rows, cols = (h, w)
-    arr = [[0 for i in range(cols)] for j in range(rows)]
-    k = [0, 0]
-    w = 0
-    h = 0
+    arr = [[0 for i in range(w)] for j in range(h)]
+    k = [0, 0]; w, h = 0, 0
     for c in board:
-        # print(f"{h} {w} {c}")
-        if c == '\n':
-            h += 1
-            w = 0
+        if c == '\n': h += 1; w = 0
         else:
-            if c == 'K':
-                k = [w, h]
-            if c == 'K' or c == 'P' or c == 'Q' or c == 'R' or c == 'B':
-                arr[h][w] = c
-            else:
-                arr[h][w] = '.'
+            if c == 'K': k = [w, h]
+            if c in {'K', 'P', 'Q', 'R', 'B'}: arr[h][w] = c
+            else: arr[h][w] = '.'
             w += 1
 
-    for row in arr:
-        for c in row:
-            print(c, end='')
-        print('\n', end='')
-    # print(f"K pos is {k_pos[0]} {k_pos[1]}")
-
-    checkmate = 0
+    # step = 0
+    # for row in arr:
+    #     for c in row: print(c, end='')
+    #     print(f" {h_max - step}", end='')
+    #     print('\n', end='')
+    #     step += 1
+    # step = 0
+    # for c in arr[0]:
+    #     print(f"{chr(ord('a') + step)}", end='')
+    #     step += 1
+    # print('\n', end='')
     
+    step = 0
+    for row in arr:
+        for c in row: print(c, end='')
+        print(f" {step}", end='')
+        print('\n', end='')
+        step += 1
+    step = 0
+    for c in arr[0]:
+        print(f"{step}", end='')
+        step += 1
+    print('\n', end='')
+
     l_max, r_max = k[0], w_max - (k[0] + 1)
     u_max, d_max = k[1], h_max - (k[1] + 1)
     print(f"King position: {k[0]} {k[1]}")
     print(f"max steps:\tl {l_max} r {r_max} u {u_max} d {d_max}")
 
-    if d_max >= 1 and (
-        arr[k[0] - (l_max >= 1)][k[1] + 1] == 'P' or
-        arr[k[0] + (r_max >= 1)][k[1] + 1] == 'P'
-    ):
-        print("A pawn is checking the king")
-        checkmate = 1
+    checkmate = 0
     
-    l, r, u, d = k[0], k[0], k[1], k[1]
+    if (arr[k[0] - (l_max >= 1)][k[1] + (d_max >= 1)] == 'P' or
+        arr[k[0] + (r_max >= 1)][k[1] + (d_max >= 1)] == 'P'):
+        checkmate = 1; print("A pawn is checking the king")
 
-    step = 1
-    while step <= l_max or step <= r_max or step <= u_max or step <= d_max:
-        if step <= l_max: l -= 1
-        if step <= r_max: r += 1
-        if step <= u_max: u -= 1
-        if step <= d_max: d += 1
-        print(f"step {step}:\t\tl {l} r {r} u {u} d {d}")
-        if arr[l][k[1]] == 'R' or arr[l][k[1]] == 'Q': checkmate = 1
-        elif arr[l][k[1]] != '.': l_max = step
-        if arr[r][k[1]] == 'R' or arr[r][k[1]] == 'Q': checkmate = 1
-        elif arr[r][k[1]] != '.': r_max = step
-        if arr[k[0]][u] == 'R' or arr[k[0]][u] == 'Q': checkmate = 1
-        elif arr[k[0]][u] != '.': u_max = step
-        if arr[k[0]][d] == 'R' or arr[k[0]][d] == 'Q': checkmate = 1
-        elif arr[k[0]][d] != '.': d_max = step
-        if arr[l][u] == 'B' or arr[l][u] == 'Q': checkmate = 1
-        elif arr[l][u] != '.': l_max, u_max = step, step
-        if arr[r][u] == 'B' or arr[r][u] == 'Q': checkmate = 1                       
-        elif arr[r][u] != '.': r_max, u_max = step, step
-        if arr[l][d] == 'B' or arr[l][d] == 'Q': checkmate = 1                       
-        elif arr[l][d] != '.': l_max, d_max = step, step
-        if arr[r][d] == 'B' or arr[r][d] == 'Q': checkmate = 1
-        elif arr[r][d] != '.': r_max, d_max = step, step
-        step += 1
+    for dx, dy, directions in [(-1, 0, l_max), (1, 0, r_max), (0, -1, u_max), (0, 1, d_max)]:
+        x, y = k[0], k[1]
+        for _ in range(directions):
+            x += dx; y += dy
+            if not (0 <= x < w_max and 0 <= y < h_max): break
+            if arr[y][x] in {'R', 'Q'}: checkmate = 1; print(f"Straight Check arr[{y}][{x}] = {arr[y][x]}") ; break
+            elif arr[y][x] != '.': break
 
-    if checkmate == 1:
-        return print("Success")
-    elif checkmate == 0:
-        return print("Fail")
+    for dx, dy, directions in [(-1, -1, min(l_max, u_max)), (1, -1, min(r_max, u_max)),
+                        (-1, 1, min(l_max, d_max)), (1, 1, min(r_max, d_max))]:
+        x, y = k[0], k[1]
+        for _ in range(directions):
+            x += dx; y += dy
+            if not (0 <= x < w_max and 0 <= y < h_max): break
+            if arr[y][x] in {'B', 'Q'}: checkmate = 1; print(f"Diagonal Check arr[{y}][{x}] = {arr[y][x]}") ; break
+            elif arr[y][x] != '.': break
+    
+    if checkmate == 1: return print("Success")
+    elif checkmate == 0: return print("Fail")
 # except:
     return print("Error")
 
 def main():
     board = """\
-R...
-.K.R
-....
-....\
+R.....
+.K...Q
+......
+......\
 """
     checkmate(board)
 
